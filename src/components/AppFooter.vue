75a8e9dbfba4c6e97f709eb2ed68805eccd199f0
Angelo31294 <luongoangelo1@gmail.com>
2022-12-07 17:48:08 +0100

    Add. footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,7 +5,26 @@ export default {
 </script>
 
 <template>
-  <h3>Ciao</h3>
+  <div class="container">
+    <div class="col">
+      <img src="../assets/author-logo-round.png" alt="logo" />
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.container {
+  width: var(--container-size);
+  margin: auto;
+  background-image: url(../assets/hero-07-2x.jpg);
+  background-position: initial;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding-top: 6.25rem;
+}
+
+img {
+  max-width: 6.25rem;
+  margin-left: 6.25rem;
+}
+</style>
